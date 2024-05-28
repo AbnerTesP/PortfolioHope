8dbb7e4131487802abce540e6fed95e474f1bd16
BoopIT\Abner Gonçalves <abner.goncalves@booit.pt>
2024-05-28 17:01:54 +0100

    Fix Contact-me Hero

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -9,7 +9,7 @@ export const Hero = () => {
         <h1 className={styles.title}>Olá, eu somos a Hope Chaves & Arte</h1>
         <p className={styles.description}> Chaveira habilidosa, especialista em calçados, resina e madeira. 
         Venha descobrir como trabalhamos em cada detalhe.</p>
-        <a href="https://wa.me/+3519333307097" className={styles.contactBtn} target='_blank' rel='noopener noreferrer'>Contacte-me</a>
+        <a href="https://wa.me/351933330709" className={styles.contactBtn} target='_blank' rel='noopener noreferrer'>Contacte-me</a>
       </div>  
       <div className={styles.function}>
       <h3 >Proprietária</h3>
