2dae31271dbe92a998d9f62c1589bcf13376a95b
BoopIT\Abner Gonçalves <abner.goncalves@booit.pt>
2024-05-29 17:44:54 +0100

    SizeTitle anda Size description Hero

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -6,7 +6,7 @@ export const Hero = () => {
   return (
     <section className={styles.container}>
       <div className={styles.content}>
-        <h1 className={styles.title}>Olá, eu somos a Hope Chaves & Arte</h1>
+        <h1 className={styles.title}>Olá, somos a Hope Chaves & Arte</h1>
         <p className={styles.description}> Chaveira habilidosa, especialista em calçados, resina e madeira. 
         Venha descobrir como trabalhamos em cada detalhe.</p>
         <a href="https://wa.me/351933330709" className={styles.contactBtn} target='_blank' rel='noopener noreferrer'>Contacte-me</a>
